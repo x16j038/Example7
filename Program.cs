5757dccc25009a0b25f21c13b5b35c39753170e7
x16j038 <x16j038@chiba-fjb.ac.jp>
2017-05-12 12:18:37 +0900

    Example7

--- Program.cs
+++ Program.cs
@@ -10,6 +10,39 @@ namespace Example7
     {
         static void Main(string[] args)
         {
+            //整数型
+            sbyte valueSByte = -100;
+            byte valueByte = 100;
+            short valueShort = -20000;
+            ushort valueUShort = 50000;
+            int valueInt = -1000000000;
+            uint valueUInt = 4000000000;
+            long valueLong = -800000000000;
+            ulong valueULong = 9000000000000000000;
         }
-    }
+            //実数型
+            float valueFloat = 3.1415F;
+            double valueDouble = 3.1415926535;
+
+            //文字型、文字列型
+            char valueChar = 'A';
+            string valueString = "C# PROGRAM";
+
+            //論理型
+            bool valueBool = true;
+
+            /*Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("byte :{0}",valueByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);
+            Console.WriteLine("sbyte :{0}",valueSByte);*/
+    }       
 }
